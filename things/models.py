e7ad8080a51461b127fcfdc3ed3918798029ca93
Jake Rawal (k22001853) <jake.rawal3@gmail.com>
2023-10-05 22:33:07 +0100

    Add model thing

--- things/models.py
+++ things/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+#models.Model is the parent class
+class Thing(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+    quantity = models.IntegerField()
+
